f8dde13b5e2ecd36bd9923106e260dc6e554cb7a
= <guillermo.santosba@gmail.com>
2022-02-28 21:20:29 -0600

    Added dev server

---
+++ webpack.prod.js
@@ -0,0 +1,15 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+const { merge } = require("webpack-merge");
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+    mode: "production",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name]-[hash].bundle.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ]
+})
